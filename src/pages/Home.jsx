03077e19142ea7328e6143163c1117b47c2e34eb
Tristan LaRoche <laroche11235@gmail.com>
2025-06-16 20:46:08 -0400

    picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-
+import teamImage from '../src/assets/pexels-photo-3182781.png'; 
 
 function Home() {
   return (
     <div className="container mt-5 home-container">
-      <img src="src/assets/pexels-photo-3182781.png" alt="Team working" className="img-fluid mb-4" />
+      <img src={teamImage} alt="Team working" className="img-fluid mb-4" />
       <h1 className="mb-3">Welcome to Email in a Day</h1>
       <p className="lead">
         We are a company dedicated to setting up professional email addresses for businesses within 24 hours.
